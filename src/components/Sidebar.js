1e01bb816b9ce669a3c0b73c6a6ae6e710d04b91
sdragon7 <ststefanpirot2@gmail.com>
2020-02-02 11:03:50 +0100

    Navbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,25 +1,90 @@
 
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Button } from 'reactstrap';
 import { TableProvider, TableConsumer } from './TableContext.js'
-
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+  } from 'reactstrap';
+  
+  
+  
 export default function Sidebar(props) {
-
+    
+        const [isOpen, setIsOpen] = useState(false);
+      
+        const toggle = () => setIsOpen(!isOpen);
+      
     return(
-        <div className="sidebar">
-            <TableConsumer>
-                {
+
+        <Navbar color="dark" dark expand="md">
+        <NavbarBrand href="/">Ima mesta</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+          <TableConsumer>
+                 {
                     context => {
-                        return (
-                            <>
-                                <Button className = "btn-xl" style = {{"width" : "100%"}} onClick={context.addTable} >DODAJ</Button>
-                                <Button className = "btn-xl" style = {{"width" : "100%"}} >OBRISI</Button>
+                         return (
+                             <>
+                                <NavItem style={{cursor : 'pointer'}}>
+                                    <NavLink onClick={context.addTable} >Dodaj</NavLink>
+                                </NavItem>
+                                <NavItem style={{cursor : 'pointer'}}>
+                                    <NavLink>Obrisi</NavLink>
+                                </NavItem>
                             </>
-                        )
-                    }
-                }
-            </TableConsumer>
-        </div>
+                         )
+                     }
+                 }
+             </TableConsumer>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>debilcine</NavbarText>
+        </Collapse>
+      </Navbar>
+
+
+        // <div className="sidebar">
+        //     <TableConsumer>
+        //         {
+        //             context => {
+        //                 return (
+        //                     <>
+        //                         <Button className = "btn-xl" style = {{"width" : "100%"}} onClick={context.addTable} >DODAJ</Button>
+        //                         <Button className = "btn-xl" style = {{"width" : "100%"}} >OBRISI</Button>
+        //                     </>
+        //                 )
+        //             }
+        //         }
+        //     </TableConsumer>
+        // </div>
 
     )
     }
